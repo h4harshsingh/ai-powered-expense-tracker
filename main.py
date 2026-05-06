ccedb26b1f7e8aa13f641ba09f5d1af14e54edad
Harsh Singh <h4harshsingh31@gmail.com>
2026-05-06 13:40:43 +0530

    Added new API

--- main.py
+++ main.py
@@ -3,21 +3,15 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-# Initial message
 message = "hello world"
 
-# Request body model
 class Message(BaseModel):
     new_message: str
 
-
-# GET API
 @app.get("/")
 def get_message():
     return {"message": message}
 
-
-# POST API
 @app.post("/change-message")
 def change_message(data: Message):
     global message
